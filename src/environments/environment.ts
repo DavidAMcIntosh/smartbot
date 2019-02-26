9a80a6123bcf9eed84c1c4815aaf1c38adc7321b
DavidAMcIntosh <davidmcintoshbz95@gmail.com>
2019-02-25 22:14:39 -0600

    configure backend service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  token: 'cd928e24111a40b7926d18df34435a99'
 };
 
 /*
